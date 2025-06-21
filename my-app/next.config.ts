3db30824cdfc86b422f37f697745eb25fd9a68c2
Faizan-coder5585 <fk33125585@gmail.com>
2025-06-21 18:41:12 +0530

    Update next.config.ts

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,7 +1,10 @@
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // 👈 disables ESLint errors during `vercel build`
+  },
 };
 
 export default nextConfig;
